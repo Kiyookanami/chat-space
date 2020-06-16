7829fed62dea1dcfe34723e55ed63eeb18e5d92f
Kiyookanami <bxionggu360@yahoo.co.jp>
2020-06-16 21:01:30 +0900

    add_to-scss

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     password = Faker::Internet.password(min_length: 8)
-    nickname {Faker::nickname.last_nickname}
+    nickname {Faker::Name.last_name}
     email{Faker::Internet.free_email}
     password {password}
     password_confirmation {password}
